9e81ebb5a456abeba7f1bedfd67c764a208a00f6
phuocnhatfabulous <6051071083@st.utc2.edu.vn>
2021-09-08 22:03:33 +0700

    Unfinish

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP
+{
+    class Person
+    {
+        public string hoten;
+        public int age;
+        //Constructor with parameters
+        public Person(string hoten1, int age1)
+        {
+            hoten = hoten1;
+            age = age1;
+        }
+        //Constructor without parameter
+        public Person()
+        {
+          
+        }
+
+        static void Main(string[] args)
+        {
+            Person a = new Person("Phuoc Nhat", 21);
+            Console.WriteLine(a.hoten + " " + a.age);
+
+            Console.ReadKey(); 
+        }
+    }
+}
