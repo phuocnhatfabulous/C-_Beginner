9c07b6b15bbab799d1a7ed7dc8467aa8ba675ecd
phuocnhatfabulous <6051071083@st.utc2.edu.vn>
2021-09-09 22:16:49 +0700

    Unfinish

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -2,28 +2,54 @@
 
 namespace OOP
 {
+    //Class 1
     class Person
     {
-        public string hoten;
+        public string name;
         public int age;
+        public string gender;
         //Constructor with parameters
         public Person(string hoten1, int age1)
         {
-            hoten = hoten1;
+            name = hoten1;
             age = age1;
         }
         //Constructor without parameter
         public Person()
         {
-          
+            name = "Doan Phuoc Nhat";
+            age = 21;
         }
+        //Info
+        public void Screen()
+        {
+            Console.WriteLine("Ho ten = {0}, Tuoi = {1}", name, age);
+            Console.WriteLine("Ho ten:" + name +"; "+ "Tuoi:" + age);
+        }
+    }
+    //Class 2
+    class Teacher : Person
+    {
+        public long salary;
+        public string subject;
 
+    }
+    //Class 3
+    class Student : Person
+    {
+        public double point;
+        public string types;
+
+
+    }
+    //Class 4
+    class Program
+    {
         static void Main(string[] args)
         {
-            Person a = new Person("Phuoc Nhat", 21);
-            Console.WriteLine(a.hoten + " " + a.age);
-
-            Console.ReadKey(); 
+            Person a = new Person();
+            a.Screen();
+            Console.ReadKey();
         }
     }
 }
